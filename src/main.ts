83124dcccf5888291297ba05b2b8214869355807
Gulshan196 <109276430+Gulshan196@users.noreply.github.com>
2023-01-13 14:02:15 +0530

    wrking on cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(
-    {origin : "*",
+    {origin : ["http://localhost:3000"],
   allowedHeaders : "*"}
   );
   await app.listen(process.env.PORT || 8080);
