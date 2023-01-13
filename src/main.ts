7d9db41255ed1690e4641d8466606134b60d9483
Gulshan196 <109276430+Gulshan196@users.noreply.github.com>
2023-01-13 13:58:09 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(
+    {origin : "*",
+  allowedHeaders : "*"}
+  );
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
